60b06f7499bba33098bf250c038724e5f3cd5900
lienputnina <liene.putnina20@gmail.com>
2025-02-25 15:54:56 +0200

    chore: added unit testing setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,19 @@
 import { NextPage } from 'next';
 import './page.scss';
-import { FoodForm } from '@/components/Form/FoodForm';
-import { Title, TitleLevel } from '@/components/Title/Title';
+import { Title, TitleAlignment, TitleLevel } from '@/components/Title/Title';
 
-// todo - create "results" page to show after submitting the form
 // todo - create a header/menu
 
 const Home: NextPage = () => {
   return (
     <main>
-      <div id="form-description">
-        <Title
-          level={TitleLevel.ONE}
-          style={{ fontSize: '50px', lineHeight: '60px', margin: '0' }}
-        >
-          Favorite food survey
-        </Title>
-        <p className="page-text">
-          This is a survey to find out the national dishes of the employees for
-          an international food day at some company.
-        </p>
-      </div>
-      <FoodForm />
+      <Title
+        level={TitleLevel.ONE}
+        style={{ fontSize: '30px', lineHeight: '30px', margin: '0' }}
+        alignment={TitleAlignment.CENTER}
+      >
+        New content will go here
+      </Title>
     </main>
   );
 };
