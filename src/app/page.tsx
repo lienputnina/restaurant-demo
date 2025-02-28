cd932f18b83eb8fabc797421a6fcc3e4642d2f4a
lienputnina <liene.putnina20@gmail.com>
2025-02-28 14:03:22 +0200

    chore: basic main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@ import { NextPage } from 'next';
 import './page.scss';
 import { Header } from 'src/components/Header/Header';
 
-// todo - create a header/menu
-
 const Home: NextPage = () => {
   return (
     <>
       <Header />
+      <main className="m-8 flex flex-col">
+        <div className="children">
+          <p>Hero image</p>
+          <p>Search bar</p>
+          <p>Other child</p>
+        </div>
+      </main>
     </>
   );
 };
