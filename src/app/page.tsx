d85dba27d6e074ada628c6a734b6c2662cc47df1
lienputnina <liene.putnina20@gmail.com>
2025-03-04 15:18:51 +0200

    fead: added the search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+'use client';
+
 import { NextPage } from 'next';
 import './page.scss';
 import { Header } from 'src/components/Header/Header';
 import { Hero } from 'src/components/Hero/Hero';
+import { TextInput } from 'src/components/TextInput/TextInput';
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <main className="flex flex-col justify-center ">
-        <div className="children">
-          <Hero />
-          <p>Search bar</p>
-          <p>Other child</p>
+      <main className="w-full ">
+        <Hero />
+        <div className="flex flex-col items-center p-6">
+          <TextInput
+            id="food-search"
+            label="Browse the foods in our menu:"
+            value=""
+            onChange={(newValue: string) => {
+              console.log(newValue);
+            }}
+          />
         </div>
       </main>
     </>
