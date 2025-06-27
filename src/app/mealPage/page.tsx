4706fc187f84227417669be0252394d36c515813
lienputnina <liene.putnina20@gmail.com>
2025-06-27 11:51:49 +0300

    refactor: updated the single meal page

---
+++ src/app/mealPage/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import {
+  Title,
+  TitleAlignment,
+  TitleLevel,
+} from '../../components/Title/Title';
+import { Meals } from 'src/components/Meals/Meals';
+
+import { NextPage } from 'next';
+import { RootState } from '../../state/store';
+import { useSelector } from 'react-redux';
+import './mealPage.scss';
+import { Header } from 'src/components/Header/Header';
+
+const SingleDish: NextPage = () => {
+  const meals = useSelector((state: RootState) => state.meals);
+
+  return (
+    <>
+      <Header />
+      <div className="w-full m-6 flex flex-col items-center">
+        <Title
+          level={TitleLevel.ONE}
+          alignment={TitleAlignment.CENTER}
+          style={{ fontSize: '50px', lineHeight: '50px', margin: '0' }}
+        >
+          {/* what is the problem? */}
+          {/* {meals.meals[0].name} */}
+          Meal name
+        </Title>
+        {/* see, if this works */}
+        <Meals meals={meals.meals} />
+      </div>
+    </>
+  );
+};
+
+export default SingleDish;
